b8d7fd172c687b9182a435479d36063e6ef88e70
Zhen Yu <iamyourfriendforever127@protonmail.com>
2021-05-17 12:28:21 +0400

    updated ocr translate

--- src/ocr_translate.py
+++ src/ocr_translate.py
@@ -7,14 +7,16 @@ from textblob import TextBlob
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="path to image")
 ap.add_argument(
-    "-l", "--lang", type=str, default="es", help="language to translate OCR'd text to (default is Spanish)"
+    "-l", "--lang", type=str, default="en", help="language that Tesseract will use when OCR'ing(default is English)"
 )
-
+ap.add_argument("-t", "--to", type=str, default="es", help="language to translate OCR'd text to (default is Spanish)")
+ap.add_argument("-p", "--psm", type=int, default=13, help="Tesseract PSM mode")
 args = ap.parse_args()
 image = cv2.imread(args.image)
-rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-text = pytesseract.image_to_string(rgb)
+options = f"-l {args.lang} --psm {args.psm}"
+text = pytesseract.image_to_string(image, config=options)
 text = text.replace("\n", " ")
 # show the original OCR'd text
 print("ORIGINAL")
@@ -22,7 +24,7 @@ print("========")
 print(text)
 
 tb = TextBlob(text)
-translated = tb.translate(to=args.lang)
+translated = tb.translate(to=args.to)
 print("TRANSLATED")
 print("==========")
 print(translated)
